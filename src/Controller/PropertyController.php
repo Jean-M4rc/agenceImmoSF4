03d4258525f419aa28c5df77e5648360800b45f2
Jean-Marc Voisin <jeanmarc.voisin.tai@gmail.com>
2019-08-17 17:42:59 +0200

    Mise en place de l'entité Property. Du Manager de Property Et ajout de deux migrations.

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,6 +17,23 @@ class PropertyController extends AbstractController
      */
     public function index(): Response
     {
+        $property = new Property();
+        $property->setTitle('Mon premier bien')
+            ->setPrice(200000)
+            ->setRooms(4)
+            ->setBedrooms(3)
+            ->setDescription('Une petite description')
+            ->setSurface(60)
+            ->setFloor(4)
+            ->setHeat(1)
+            ->setCity('Montpellier')
+            ->setAddress('15Boulevard Gambetta')
+            ->setPostalCode('34000');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($property);
+        $em->flush();
+
         return $this->render('property/index.html.twig', [
             'current_menu'=> 'properties'
         ]);
